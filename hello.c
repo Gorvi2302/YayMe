11689b4197d9e8a6db26b39b3510fd482dc8690f
Gorvi2302 <66692107+Gorvi2302@users.noreply.github.com>
2020-06-09 23:44:03 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,2 +1,28 @@
 #include<stdio.h>
-printf("Hello!");
+main()
+{
+  int marks;
+  printf("Enter marks: ");
+  scanf("%d",&marks);
+  if(marks>84 && marks<101)
+  {
+    printf("Grade A");
+  }
+  else if(marks>69 && marks<85)
+  {
+    printf("Grade B");
+  }
+    else if(marks>54 && marks<70)
+    { 
+      printf("Grade C");
+     }
+     else if(marks>39 && marks<55)
+     {
+       printf("Grade D");
+     }
+     else
+             {
+               printf("Grade F");
+     }
+}
+      

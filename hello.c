39b7a7a37db136383ae3ce834421970a801a3c19
Gorvi2302 <66692107+Gorvi2302@users.noreply.github.com>
2020-06-12 21:27:46 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,28 +1,14 @@
 #include<stdio.h>
 main()
 {
-  int marks;
-  printf("Enter marks: ");
-  scanf("%d",&marks);
-  if(marks>84 && marks<101)
-  {
-    printf("Grade A");
-  }
-  else if(marks>69 && marks<85)
-  {
-    printf("Grade B");
-  }
-    else if(marks>54 && marks<70)
-    { 
-      printf("Grade C");
-     }
-     else if(marks>39 && marks<55)
-     {
-       printf("Grade D");
-     }
-     else
-             {
-               printf("Grade F");
-     }
+  int a,b;
+printf("Enter number 1: ");
+scanf("%d",&a);
+printf("Enter number 2: ");
+scanf("%d",&b);
+a+=b;
+b=a-b;
+a-=b;
+printf("Now number 1 is %d and number 2 is %d",a,b);
 }
       
